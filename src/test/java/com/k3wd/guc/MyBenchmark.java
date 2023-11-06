ff05ea2d5311af3dd48b5c28880678428cc6a9a0
k3wd <k3wd1999@163.com>
2023-11-06 23:03:49 +0800

    workbench

---
+++ src/test/java/com/k3wd/guc/MyBenchmark.java
@@ -0,0 +1,87 @@
+package com.k3wd.guc;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author k3wd
+ * @date 2023/11/6
+ */
+@Fork(1)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+public class MyBenchmark {
+    static int[] ARRAY = new int[1000_000_00];
+
+    static {
+        Arrays.fill(ARRAY, 1);
+    }
+
+    @Benchmark
+    public int c() throws Exception {
+        int[] array = ARRAY;
+        FutureTask<Integer> t1 = new FutureTask<>(() -> {
+            int sum = 0;
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[0 + i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t2 = new FutureTask<>(() -> {
+            int sum = 0;
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[250_000_00 + i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t3 = new FutureTask<>(() -> {
+            int sum = 0;
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[500_000_00 + i];
+            }
+            return sum;
+        });
+        FutureTask<Integer> t4 = new FutureTask<>(() -> {
+            int sum = 0;
+            for (int i = 0; i < 250_000_00; i++) {
+                sum += array[750_000_00 + i];
+            }
+            return sum;
+        });
+        new Thread(t1).start();
+        new Thread(t2).start();
+        new Thread(t3).start();
+        new Thread(t4).start();
+        return t1.get() + t2.get() + t3.get() + t4.get();
+    }
+    
+    @Benchmark
+    public int d() throws Exception {
+        int[] array = ARRAY;
+        FutureTask<Integer> t1 = new FutureTask<>(() -> {
+            int sum = 0;
+            for (int i = 0; i < 1000_000_00; i++) {
+                sum += array[0 + i];
+            }
+            return sum;
+        });
+        new Thread(t1).start();
+        return t1.get();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(MyBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
